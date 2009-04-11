fea6bbaacf548adfa745103f3b68ce7a603682f3
Tony Hillerson <tony.hillerson@effectiveui.com>
2009-04-11 15:12:07 -0600

    simple user test; added joda time as a dep

--- src/main/scala/twitter4s/domain/User.scala
+++ src/main/scala/twitter4s/domain/User.scala
@@ -1,7 +1,38 @@
 package twitter4s.domain
 
 import _root_.scala.xml.NodeSeq
+import java.lang.Boolean._
+import java.lang.Integer._
+import java.lang.Long._
+import org.joda.time._;
+import org.joda.time.format._;
 
 class User(x:NodeSeq) {
-    def name = (x \ "name").text
+
+  val STD_DATE_TIME_FORMATTER = DateTimeFormat.forPattern("EEE MMM dd HH:mm:ss Z yyyy");
+
+  def id                        = parseLong((x \ "id").text)
+  def name                      = (x \ "name").text
+  def screenName                = (x \ "screen_name").text
+  def location                  = (x \ "location").text
+  def description               = (x \ "description").text
+  def url                       = (x \ "url").text
+  def isProtected               = parseBoolean(((x \ "protected").text))
+  def notifications             = parseBoolean((x \ "notifications").text)
+  def following                 = parseBoolean((x \ "following").text)
+  def followersCount            = parseInt((x \ "followers_count").text)
+  def friendsCount              = parseInt((x \ "friends_count").text)
+  def favouritesCount           = parseInt((x \ "favourites_count").text)
+  def statusesCount             = parseInt((x \ "statuses_count").text)
+  def createdAt                 = STD_DATE_TIME_FORMATTER.parseDateTime((x \ "created_at").text)
+  def utcOffset                 = parseInt((x \ "utc_offset").text)
+  def timeZone                  = (x \ "time_zone").text
+  def profileImageURL           = (x \ "profile_image_url").text
+  def profileBackgroundColor    = (x \ "profile_background_color").text
+  def profileTextColor          = (x \ "profile_text_color").text
+  def profileLinkColor          = (x \ "profile_link_color").text
+  def profileSidebarFillColor   = (x \ "profile_sidebar_fill_color").text
+  def profileSidebarBorderColor = (x \ "profile_sidebar_border_color").text
+  def profileBackgroundImageURL = (x \ "profile_background_image_url").text
+  def profileBackgroundTile     = parseBoolean((x \ "profile_background_tile").text)
 }
--- src/test/scala/twitter4s/TwitterObjectsTest.scala
+++ src/test/scala/twitter4s/TwitterObjectsTest.scala
@@ -2,6 +2,7 @@ package twitter4s
 
 import org.specs._
 import org.specs.runner.{ConsoleRunner, JUnit4}
+import org.joda.time._;
 import scala.xml._
 
 import twitter4s.domain._
@@ -12,10 +13,67 @@ object TwitterObjectTestRunner extends ConsoleRunner(TwitterObjectsTest)
 object TwitterObjectsTest extends Specification {
 
   "A User" should {
-    "have a name" in {
-      val user = new User(XML.loadString("<user><name>Bob Dobbs</name></user>"))
-      user.name must beMatching("Bob Dobbs")
+    "deserialize from xml" in {
+      val user = new User(userXML)
+      user.id must_== 5815992L
+      user.name must beMatching("Tony Hillerson")
+      user.screenName must beMatching("thillerson")
+      user.location must beMatching("Arvada")
+      user.description must beMatching("I'm a bass player, electronic music maker, developer for effectiveUI, flash, flex, rails, java programmer, and I fancy myself a philosoper of sorts.")
+      user.profileImageURL must beMatching("http://s3.amazonaws.com/twitter_production/profile_images/52156522/Photo_3_normal.jpg")
+      user.url must beMatching("http://thillerson.blogspot.com")
+      user.notifications must beFalse
+      user.following must beFalse
+      user.isProtected must beFalse
+      user.followersCount must_== 440
+      user.friendsCount must_== 332
+      user.statusesCount must_== 3599
+      user.profileBackgroundColor must beMatching("1A1B1F")
+      user.profileTextColor must beMatching("666666")
+      user.profileLinkColor must beMatching("2FC2EF")
+      user.profileSidebarFillColor must beMatching("252429")
+      user.profileSidebarBorderColor must beMatching("181A1E")
+      user.profileBackgroundImageURL must beMatching("http://static.twitter.com/images/themes/theme9/bg.gif")
+      user.profileBackgroundTile must beFalse
+      user.utcOffset must_== -25200
+      user.timeZone must beMatching("Mountain Time.*")
+      user.timeZone must beMatching(".*(US & Canada)")
+      user.createdAt.isEqual(new DateTime(2007, 5, 6, 21, 05, 23, 0, DateTimeZone.UTC)) must beTrue
     }
   }
+
+  val userXML = 
+  <user>
+    <id>5815992</id>
+    <name>Tony Hillerson</name>
+    <screen_name>thillerson</screen_name>
+    <location>Arvada</location>
+    <description>
+      I'm a bass player, electronic music maker, developer for effectiveUI, flash, flex, rails, java programmer, and I fancy myself a philosoper of sorts.
+    </description>
+    <profile_image_url>
+      http://s3.amazonaws.com/twitter_production/profile_images/52156522/Photo_3_normal.jpg
+    </profile_image_url>
+    <url>http://thillerson.blogspot.com</url>
+    <protected>false</protected>
+    <followers_count>440</followers_count>
+    <profile_background_color>1A1B1F</profile_background_color>
+    <profile_text_color>666666</profile_text_color>
+    <profile_link_color>2FC2EF</profile_link_color>
+    <profile_sidebar_fill_color>252429</profile_sidebar_fill_color>
+    <profile_sidebar_border_color>181A1E</profile_sidebar_border_color>
+    <friends_count>332</friends_count>
+    <created_at>Sun May 06 21:05:23 +0000 2007</created_at>
+    <favourites_count>3</favourites_count>
+    <utc_offset>-25200</utc_offset>
+    <time_zone>Mountain Time &#40;US &amp; Canada&#41;</time_zone>
+    <profile_background_image_url>
+      http://static.twitter.com/images/themes/theme9/bg.gif
+    </profile_background_image_url>
+    <profile_background_tile>false</profile_background_tile>
+    <statuses_count>3599</statuses_count>
+    <notifications>false</notifications>
+    <following>false</following>
+  </user>
   
 }
